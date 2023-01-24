f39b50fe23c0aa7d04ea53ca9ec65c6ef0b421ab
Stephen Pellicer <spellicer@gmail.com>
2023-01-23 19:39:25 -0500

    format and unset FIXMEs

--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -4,12 +4,31 @@
 
 package frc.robot.subsystems;
 
+import static frc.robot.Constants.BACK_LEFT_MODULE_DRIVE_MOTOR;
+import static frc.robot.Constants.BACK_LEFT_MODULE_STEER_ENCODER;
+import static frc.robot.Constants.BACK_LEFT_MODULE_STEER_MOTOR;
+import static frc.robot.Constants.BACK_LEFT_MODULE_STEER_OFFSET;
+import static frc.robot.Constants.BACK_RIGHT_MODULE_DRIVE_MOTOR;
+import static frc.robot.Constants.BACK_RIGHT_MODULE_STEER_ENCODER;
+import static frc.robot.Constants.BACK_RIGHT_MODULE_STEER_MOTOR;
+import static frc.robot.Constants.BACK_RIGHT_MODULE_STEER_OFFSET;
+import static frc.robot.Constants.DRIVETRAIN_PIGEON_ID;
+import static frc.robot.Constants.DRIVETRAIN_TRACKWIDTH_METERS;
+import static frc.robot.Constants.DRIVETRAIN_WHEELBASE_METERS;
+import static frc.robot.Constants.FRONT_LEFT_MODULE_DRIVE_MOTOR;
+import static frc.robot.Constants.FRONT_LEFT_MODULE_STEER_ENCODER;
+import static frc.robot.Constants.FRONT_LEFT_MODULE_STEER_MOTOR;
+import static frc.robot.Constants.FRONT_LEFT_MODULE_STEER_OFFSET;
+import static frc.robot.Constants.FRONT_RIGHT_MODULE_DRIVE_MOTOR;
+import static frc.robot.Constants.FRONT_RIGHT_MODULE_STEER_ENCODER;
+import static frc.robot.Constants.FRONT_RIGHT_MODULE_STEER_MOTOR;
+import static frc.robot.Constants.FRONT_RIGHT_MODULE_STEER_OFFSET;
+
 import com.ctre.phoenix.sensors.PigeonIMU;
-import com.swervedrivespecialties.swervelib.Mk3SwerveModuleHelper;
-import com.swervedrivespecialties.swervelib.Mk4ModuleConfiguration;
 import com.swervedrivespecialties.swervelib.Mk4SwerveModuleHelper;
 import com.swervedrivespecialties.swervelib.SdsModuleConfigurations;
 import com.swervedrivespecialties.swervelib.SwerveModule;
+
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
@@ -19,8 +38,6 @@ import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-import static frc.robot.Constants.*;
 
 public class DrivetrainSubsystem extends SubsystemBase {
         /**
@@ -73,11 +90,7 @@ public class DrivetrainSubsystem extends SubsystemBase {
         // The important thing about how you configure your gyroscope is that rotating
         // the robot counter-clockwise should
         // cause the angle reading to increase until it wraps back over to zero.
-        // FIXME Remove if you are using a Pigeon
         private final PigeonIMU m_pigeon = new PigeonIMU(DRIVETRAIN_PIGEON_ID);
-        // FIXME Uncomment if you are using a NavX
-        // private final AHRS m_navx = new AHRS(SPI.Port.kMXP, (byte) 200); // NavX
-        // connected over MXP
 
         // These are our modules. We initialize them in the constructor.
         private final SwerveModule m_frontLeftModule;
@@ -113,7 +126,6 @@ public class DrivetrainSubsystem extends SubsystemBase {
                 // By default we will use Falcon 500s in standard configuration. But if you use
                 // a different configuration or motors
                 // you MUST change it. If you do not, your code will crash on startup.
-                // FIXME Setup motor configuration
                 m_frontLeftModule = Mk4SwerveModuleHelper.createNeo(
                                 // This parameter is optional, but will allow you to see the current state of
                                 // the module on the dashboard.
@@ -170,26 +182,11 @@ public class DrivetrainSubsystem extends SubsystemBase {
          * 'forwards' direction.
          */
         public void zeroGyroscope() {
-                // FIXME Remove if you are using a Pigeon
                 m_pigeon.setFusedHeading(0.0);
-
-                // FIXME Uncomment if you are using a NavX
-                // m_navx.zeroYaw();
         }
 
         public Rotation2d getGyroscopeRotation() {
-                // FIXME Remove if you are using a Pigeon
                 return Rotation2d.fromDegrees(m_pigeon.getFusedHeading());
-
-                // FIXME Uncomment if you are using a NavX
-                // if (m_navx.isMagnetometerCalibrated()) {
-                // // We will only get valid fused headings if the magnetometer is calibrated
-                // return Rotation2d.fromDegrees(m_navx.getFusedHeading());
-                // }
-                //
-                // // We have to invert the angle of the NavX so that rotating the robot
-                // counter-clockwise makes the angle increase.
-                // return Rotation2d.fromDegrees(360.0 - m_navx.getYaw());
         }
 
         public void drive(ChassisSpeeds chassisSpeeds) {
